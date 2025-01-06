ea0f0d10a3b82c426db736addbf5a980df951f59
arungeorgesaji <arungeorgesaji@gmail.com>
2025-01-06 13:50:24 +0530

    Seems to be a unexpected  change in pulse so changed it back

--- pulse.py
+++ pulse.py
@@ -1,28 +1,27 @@
-from gpiozero import Button
 import pulsectl
+import threading
+import time
 
-class VolumeControl:
-    def __init__(self, button_pin):
+class VolumeControl(threading.Thread):
+    def __init__(self, button):
+        super().__init__()
         self.pulse = pulsectl.Pulse('volume-control')
-        self.button = Button(button_pin)
+        self.button = button
+        self._stop_event = threading.Event()
 
-        self.button.when_pressed = self.button_pressed
-        self.button.when_released = self.button_released
+    def run(self):
+        while not self._stop_event.is_set():
+            if self.button.is_pressed:
+                self.set_volume(1.0)
+            else:
+                self.set_volume(0.0)
+            time.sleep(1)  
+
+    def stop(self):
+        self._stop_event.set()
 
     def set_volume(self, volume):
         sinks = self.pulse.sink_list()
         for sink in sinks:
             self.pulse.volume_set_all_chans(sink, volume)
-
-    def button_pressed(self):
-        self.set_volume(1.0)  
-
-    def button_released(self):
-        self.set_volume(0.0)  
-
-    def run(self):
-        try:
-            while True:
-                pass  
-
 
